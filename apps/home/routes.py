aeeaa4e57d8b83026a9a59611540b9fa3df069da
jgutierrezre <jgutierrezre@unal.edu.co>
2021-11-24 20:56:17 -0500

    Getting rid of all bloat.

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,23 +1,17 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from apps.home import blueprint
 from flask import render_template, request
-from flask_login import login_required
 from jinja2 import TemplateNotFound
 
+@blueprint.route('/')
+def route_default():
+    return render_template('home/index.html', segment='index')
 
 @blueprint.route('/index')
-@login_required
 def index():
-
     return render_template('home/index.html', segment='index')
 
 
 @blueprint.route('/<template>')
-@login_required
 def route_template(template):
 
     try:
--- run.py
+++ run.py
@@ -1,14 +1,9 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from flask_migrate import Migrate
 from sys import exit
 from decouple import config
 
 from apps.config import config_dict
-from apps import create_app, db
+from apps import create_app
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
@@ -17,7 +12,6 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 get_config_mode = 'Debug' if DEBUG else 'Production'
 
 try:
-
     # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
 
@@ -25,12 +19,11 @@ except KeyError:
     exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
 
 app = create_app(app_config)
-Migrate(app, db)
+Migrate(app)
 
 if DEBUG:
     app.logger.info('DEBUG       = ' + str(DEBUG))
     app.logger.info('Environment = ' + get_config_mode)
-    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
 
 if __name__ == "__main__":
     app.run()
